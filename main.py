1d7eeaa28bba5119272dbd52faf8f8d9ad775788
mak98 <mayank.m998@gmail.com>
2023-04-30 23:43:18 -0700

    UI changes and code clean

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request, Form,File, UploadFile
+from fastapi import FastAPI, Request, Form,File, UploadFile,HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBasic
+from starlette.middleware.sessions import SessionMiddleware
 from PIL import Image
 import os
 import io
@@ -12,9 +13,11 @@ import mysql.connector
 import json
 
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="SECRET_KEY")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 security = HTTPBasic()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 mydb = mysql.connector.connect(
     host="localhost",
@@ -27,6 +30,10 @@ modelHandeler=Models("./static/uploads/")
 # Login page route
 @app.get("/")
 def login(request: Request):
+        if "flash" in request.session.keys():
+            body={"request": request,"error":request.session["flash"]}
+            request.session.pop("flash",None)
+            return templates.TemplateResponse("login.html",body )
         return templates.TemplateResponse("login.html", {"request": request})
 # Authentication route
 @app.post("/login")
@@ -35,13 +42,13 @@ def authenticate_user(request: Request, email: str = Form(...), password: str = 
     val = (email,)
     mycursor.execute(sql, val)
     user = mycursor.fetchone()
-    print(val,user[0])
     if not user:
-        print("lol")
+        request.session["flash"]="User Credentials Invalid"
         return RedirectResponse(url="/",status_code=303,headers={"x-error":"Invalid User Credentials"})
     if not pwd_context.verify(password, user[0]):
-        print("kd")
+        request.session["flash"]="User Credentials Invalid"
         return RedirectResponse(url="/",status_code=303,headers={"x-error":"Invalid User Credentials"})
+    request.session["user"]=email
     return RedirectResponse(url="/dashboard",status_code=303)
 
 # Signup page route
@@ -64,16 +71,24 @@ def add_user(request: Request, fname: str = Form(...),lname: str = Form(...),ema
     val = (email, hashed_password,fname,lname)
     mycursor.execute(sql, val)
     mydb.commit()
+    request.session["user"]=email
     return RedirectResponse(url="/dashboard",status_code=303)
-
+@app.get("/logout")
+def logout(request:Request):
+    if "user" in request.session.keys():
+        request.session.pop("user",None)
+        return RedirectResponse(url="/")
 @app.get("/dashboard")
 def dashoard(request:Request):
-    #print(request.session["user"])
+    if "user" not in request.session.keys():
+        return RedirectResponse(url="/",status_code=303)
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
 #Upload access point for models and front end
 @app.get("/upload")
 async def card1(request: Request):
+    if "user" not in request.session.keys():
+        return RedirectResponse(url="/")
     return templates.TemplateResponse("upload.html", {"request": request})
 @app.post("/upload")
 async def upload_file(pt_file: UploadFile = File(...)):
@@ -87,12 +102,14 @@ async def upload_file(pt_file: UploadFile = File(...)):
     print(file_path)
     modelHandeler.loadModel(pt_file.filename,file_path)
     print(modelHandeler.getModel())
-    return {"filename": pt_file.filename}
+    return RedirectResponse("/manage",status_code=303)
 
 
 #Manage ML accesspoints
 @app.get("/manage")
 async def card2(request: Request):
+    if "user" not in request.session.keys():
+        return RedirectResponse(url="/")
     models=modelHandeler.getModel()
     return templates.TemplateResponse("manage.html", {"request": request,"models":models})
 @app.get("/delete/{mname}")
@@ -103,7 +120,8 @@ async def delete_item(request: Request, mname: str):
 #Call Prediction 
 @app.post("/predict/{mname}")
 async def predict(request: Request,mname:str,file: UploadFile = File(...)):
-    print(request,mname)
+    if mname not in modelHandeler.getModel():
+        raise HTTPException(status_code=404, detail="Model not found")
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
     prediction = modelHandeler.predict(mname,image)
